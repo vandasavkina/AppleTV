02f4f1f81d71255187fcfadc998b2184db28b52b
Vanda <wandasavkina@hotmail.it>
2023-11-14 16:43:16 +0100

    Initial Commit

---
+++ AppleTv/AppleTvApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTvApp.swift
+//  AppleTv
+//
+//  Created by Vanda Savkina on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleTvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
