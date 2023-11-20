165b7ff70b51681d8997157bf2c538ccac4986a3
vandasavkina <wandasavkina@hotmail.it>
2023-11-20 16:50:30 +0100

    Add README.md

---
+++ AppleTv/Carousel.swift
@@ -0,0 +1,18 @@
+//
+//  Carousel.swift
+//  AppleTv
+//
+//  Created by Vanda Savkina on 23/11/23.
+//
+
+import SwiftUI
+
+struct Carousel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Carousel()
+}
---
+++ AppleTv/Film/Carousel.swift
@@ -0,0 +1,8 @@
+//
+//  Carousel.swift
+//  AppleTv
+//
+//  Created by Vanda Savkina on 23/11/23.
+//
+
+import Foundation
